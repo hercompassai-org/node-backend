c99e07ade75b1a2cd7f896334acfc4f6bee64977
ski12345491 <abu@ascinatetech.com>
2025-12-10 12:23:59 +0530

    final push

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://progressive-hercompass.vercel.app"
+      "http://hercompassai.com"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept", "X-Requested-With"],
