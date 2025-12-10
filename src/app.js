b7462f38b412c155654871afaf351ccfc23010f0
ski12345491 <abu@ascinatetech.com>
2025-12-10 12:43:35 +0530

    new cors error

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "http://hercompassai.com"
+      "https://www.hercompassai.com/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept", "X-Requested-With"],
